93254d0d6b802bfd20210152e8dd45c691ba529f
Bartosz Legięć <bartosz@legiec.eu>
2018-02-17 12:18:34 +0100

    Added server

---
+++ server/index.js
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-commonjs */
+const jsonServer = require("json-server");
+const path = require("path");
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+// Add custom routes before JSON Server router
+server.post("/echo", (req, res) => {
+  res.jsonp(req.body);
+});
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+
+// Use default router
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
